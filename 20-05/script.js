9b495c482162d85cf4dac577cf901ae18ca28159
Rghvs03 <raghav437@gmail.com>
2025-05-21 19:35:47 +0530

    21-05

---
+++ 20-05/script.js
@@ -0,0 +1,62 @@
+// var n = prompt();
+
+// for(let i=1;i<=n;i++){
+//    if(isNaN(n) || n === null || n.trim() === ""){
+//     console.error("invalid input");
+//    }
+//    else{
+//     console.log(i);
+//    }
+             
+   
+// }
+
+//  var n = prompt();
+//  if(isNaN(n) || n === null || n.trim() === ""){
+//      console.error("invalid input");
+    
+//  }
+//  else{
+//     for(let i = 1; i<= n;i++){
+//     console.log(i);
+// }
+//  }
+
+
+// Push 
+// pop 
+// shift 
+// unshift 
+
+// ek loop chalao 5 baar har baar prompt mange jo bhi prompt me de de voh array me save ho jaye last me pura array console mai print ho jaaye 
+ 
+let arr = [];
+for(let i = 0; i < 5; i++){
+ 
+   arr.push(prompt("Enter the value"));
+   
+}
+console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1
